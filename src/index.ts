7f2d48ccbdd05132dfb305bba321275145c0cda1
Fabio Henriques <fabiohvp@hotmail.com>
2021-10-24 16:17:41 -0300

    remove unused files change startup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 export * from './OAuthResponse';
 export * from './OAuthSettings';
 export * from './OAuthHandleInput';
-export * from './OAuthPassportHandler';
 export * from './OAuthCookie';
 export * from './OAuthSession';
 export * from './OAuthApis';
 export * from './ExpressUtils';
+import type { OAuthSetting } from './OAuthSettings';
+import passport from 'passport';
+
+export function ConfigurePassportOAuth2(oauthSettings: OAuthSetting[]): void {
+	for (const setting of oauthSettings) {
+		passport.use(setting.strategy);
+	}
+}
